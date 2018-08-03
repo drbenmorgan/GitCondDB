1ab742d389ebbc077fd73a0112fa3eee70a5bcd2
Marco Clemencic <marco.clemencic@cern.ch>
2018-08-03 15:33:28 +0200

    Attempt for a CMakeLists.txt building shared and static library

--- include/GitCondDB.h
+++ include/GitCondDB.h
@@ -1,17 +1,19 @@
 #ifndef GITCONDDB_H
 #define GITCONDDB_H
 
+#include <gitconddb_export.h>
+
 #include <string_view>
 
 namespace GitCondDB
 {
   inline namespace v1
   {
-    class CondDB
+    class GITCONDDB_EXPORT CondDB
     {
     };
 
-    CondDB connect( std::string_view repository );
+    GITCONDDB_EXPORT CondDB connect( std::string_view repository );
   }
 }
 
